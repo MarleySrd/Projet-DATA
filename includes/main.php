c51b3a276506362956e0124829cc75b824e54a3a
laurentbasset <l.basset@codeur.online>
2021-09-22 14:39:49 +0200

    préparation du bloc  main

--- includes/main.php
+++ includes/main.php
@@ -3,7 +3,7 @@
         <article id="genre">genres</article>
         <article id="tops">top</article>
         <article id="discover">découverte</article>
-        <article id="discover">top</article>
+        <!-- ... -->
     </main>
     <!-- end main -->
     
