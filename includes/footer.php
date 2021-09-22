5281a0a68a6f429395b96b290ce7f568c96e4a16
laurentbasset <l.basset@codeur.online>
2021-09-22 14:16:52 +0200

    first architecture

---
+++ includes/footer.php
@@ -0,0 +1,52 @@
+    <!-- footer -->
+    <footer>
+        <div id="col1">
+            <div class="headerCol">
+                <div class="logo">
+                    <div class="trait4"></div>
+                    <div class="trait5"></div>
+                    <div class="trait6"></div>
+                    <div class="trait7"></div>
+                    <div class="trait8"></div>
+                    <div class="trait9"></div>
+                    <div class="trait10"></div>
+                </div>
+                <div class="brand">
+                    <div class="title">Music Band</div>
+                    <div class="subtitle">La music au bout des doigts</div>
+                </div>
+            </div>
+            <p class="txt">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed ante sit amet mi fermentum accumsan. Nulla tristique, lorem eget accumsan efficitur, enim ipsum venenatis massa, mattis eleifend dui nibh sed sem. Vivamus nec quam turpis. Integer auctor leo at convallis malesuada. Praesent cursus feugiat ligula et tristique. Vivamus in velit nec nibh venenatis semper id eu ligula. Nullam dignissim vehicula tempor. Quisque a elementum purus. Maecenas condimentum arcu non nulla feugiat pharetra. Nunc aliquam enim augue, ultricies porta nulla maximus sed.
+            </p>
+        </div>
+        <div id="col2">
+            <div class="headerCol">Inscrivez-vous à la Newsletter</div>
+            <p class="txt">Abonnez-vous à notre newsletter et recevez les dernières mises à jour et offres.</p>
+            <form id="subscribe">
+                <input type="text" placeholder="Entrez votre nom" name="nom">
+                <input type="text" placeholder="Entrez votre prénom" name="prenom">
+                <input type="submit" value="insrcrivez-vous">
+            </form>
+        </div>
+        <div id="col3">
+            
+            <div class="headerCol">Nous contacter</div>
+            <p class="txt">Vous souhaitez nous contacter.<br>
+                Plusieurs solutions s’offrent à vous</p>
+                <div class="contact">
+                    <button class="bt"><img src="img/phone.svg" alt="téléphone"></button>
+                    <div class="txt">Par téléphone au:<br><a href="tel:0389878586" title="téléphone">03 89 87 85 86</a></div>
+                </div>
+
+                <div class="contact">
+                    <button class="bt"><img src="img/mail.svg" alt="email"></button>
+                    <div class="txt">Par email au:<br><a href="mailto:contact@musicband.com" title="mail">contact[@]musicband.com</a></div>
+                </div>
+        </div>
+    </footer>
+    <!-- end footer -->
+
+    </div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,33 @@
+    <!-- header -->
+    <header>
+        <div id="headerLeft">
+            <div id="btBurger">
+                <div id="trait1"></div>
+                <div id="trait2"></div>
+                <div id="trait3"></div>
+            </div>
+            <div class="logo">
+                <div class="trait4 tBlue"></div>
+                <div class="trait5 tBlue"></div>
+                <div class="trait6 tBlue"></div>
+                <div class="trait7 tBlue"></div>
+                <div class="trait8 tBlue"></div>
+                <div class="trait9 tBlue"></div>
+                <div class="trait10 tBlue"></div>
+            </div>
+            <div class="brand">
+                <div class="title">Music Band</div>
+                <div class="subtitle">La music au bout des doigts</div>
+            </div>
+        </div>
+
+        <div id="headerRight">
+
+             <input id="search" type="search" placeholder="recherchez, écoutez..."><button id="btSearch"><img src="img/headphones.svg" alt="headphones"></button>
+          
+            <div id="login">
+                <div id="avatar"><img src="img/avatar.jpg" alt="avatar"></div><span id="name">Alain</span>
+            </div>
+        </div>
+    </header>
+    <!-- end header -->
