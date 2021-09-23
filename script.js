32b05df5740edbe86b8acf90a55f35c0aba71612
JoselitoBTW <j.payet@codeur.online>
2021-09-23 14:04:21 +0200

    genre js change

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 const dataApi = fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre");
 
 dataApi
-  .then(async (responseData) => {
-    console.log(responseData);
+    .then(async (responseData) => {
+        console.log(responseData);
 
-    const response = await responseData.json();
-    // const response = await responseData;
-    console.log(response);
-    let genre = document.querySelector('#genre');
-    try {
-      for (let i = 1; i < 11 ; i++) {
-        const dataPicture = response.data[i].picture;
-        const dataName = response.data[i].name;
-        let myImg = document.createElement('img');
-        let myTitle = document.createElement('p');
-        let myGenre = document.createElement('div');
-        myImg.src = datataPicture;
-        myTitle.textContent = datataName;
-        myGenre.appendChild(myImg);
-        myGenre.appendChild(myTitle);
-        genre.appendChild(myGenre);
-      }
+        const response = await responseData.json();
+        console.log(response);
+        try {
+            for (let i = 1; i < response.length; i++) {
+                let urlImg = response.data[i].picture;
+                let genreName = response.data[i].name;
+                let slide = createSlideAlbum(urlImg, genreName);
+                addSLideToListGenre(slide);
+            }
+            
+        } catch (err) {
+            console.log(err);
+        }
+    })
 
-    } catch (err) {
-      console.log(err);
+    .catch((err) => {
+        console.log(err)
+    })
+
+    function createSlideAlbum(imgThumb, nameGenre){
+        let slide = '<div class="albumSlide">';
+        slide += '<div class="thumbGenre"><img src="'+urlthumb+'" alt="album"></div>';
+        slide += '<div class="nameGenre">'+nameGenre+'</div>';
+        slide += '</div>';
+    
+        return slide;
+    
     }
-  })
-
-  .catch((err) => {
-    console.log(err)
-  })
+    
+    function addSLideToListGenre(slide){
+        let listGenre =  document.getElementById('wrpSlider');
+        listGenre.innerHtml += slide;
+    }
