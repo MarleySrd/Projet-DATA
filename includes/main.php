c9e3eedbe6f2d252fedc33a2f83f2536c720e5e6
laurentbasset <l.basset@codeur.online>
2021-09-23 11:31:32 +0200

    prepa js

--- includes/main.php
+++ includes/main.php
@@ -1,7 +1,55 @@
     <!-- main -->
     <main>
-        <article id="genre">genre</article>
-        <article id="tops">top</article>
+        <article id="genre">
+            <h2>genre</h2>
+            <div id="slider">
+            <!-- <ul>
+                    <li>
+                        <div class="thumbAlbum"><img src="img/thumbAlbum.jpg" alt="titleAlbum"><div>
+                        <div class="titleAlbum">Tilte Album</div>
+                    </li>
+                    <li>
+                        <div class="thumbAlbum"><img src="img/thumbAlbum.jpg" alt="titleAlbum"><div>
+                        <div class="titleAlbum">Tilte Album</div>
+                    </li>
+                </ul> -->
+            </div>
+
+        </article>
+        <article id="tops">
+            <div id="topTracks">
+                <h2><span class="blue">Top 10</span> des morceaux les plus écoutés</h2>
+                <div class="listTop">
+                    <div class="lineTop">
+                        <div class="leftLine">
+                            <div class="nbr">01</div>
+                            <div class="thumbAlbumTopTracks"><img src="img/thumbTopTrack.jpg" alt="thumb Album"></div>
+                            < class="trackInfos">
+                                <div class="TitleTrack">Title track</div>
+                                <div class="artistName blue">Artist name</div>
+                            </div>
+                        </div>
+                        <div class="timer">00:00</div>
+                    </div>
+                    
+                    <div class="lineTop">
+                        <div class="leftLine">
+                            <div class="nbr">02</div>
+                            <div class="thumbAlbumTopTracks"><img src="img/thumbTopTrack.jpg" alt="thumb Album"></div>
+                            <div class="trackInfos">
+                                <div class="TitleTrack">Title track</div>
+                                <div class="artistName blue">Artist name</div>
+                            </div>
+                        </div>
+                        <div class="timer">00:00</div>
+                    </div>
+                </div>
+            </div>
+           
+            <div id="topAlbums">
+                <h2><span class="blue">Top 10</span> des albums les plus écoutéx</h2>
+            </div>
+        </article>
         <article id="discover">découverte</article>
         <!-- ... -->
     </main>
--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 window.addEventListener('load', (event) => {
     console.log('js work !');
 });
+
+//
+function createLineTop(nbr, urlThumb, titleTrack, artistName, duration) {
+
+    let list = '<div class="lineTop">';
+    let list += '<div class="leftLine">';
+    let list += '    <div class="nbr">'+nbr+'</div>';
+    let list += '    <div class="thumbAlbumTopTracks"><img src="'+urlThumb+'" alt="thumb Album"></div>';
+    let list += '    < class="trackInfos">';
+    let list += '        <div class="TitleTrack">'+titleTrack+''</div>';
+    let list += '        <div class="artistName blue">'+artistName+'</div>';
+    let list += '    </div>';
+    let list += '</div>';
+    let list += '<div class="timer">'+duration''</div>';
+    let list += '</div>';
+
+    return list;
+
+}
+
+function creatListTopAlbum(){
+
+}
