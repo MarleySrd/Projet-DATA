615ec0f792822e4b34640d0039397d813d256f29
MarleySrd <m.sorhaindo@codeur.online>
2021-09-23 11:49:59 +0200

    albums and tracks

--- includes/main.php
+++ includes/main.php
@@ -1,9 +1,17 @@
     <!-- main -->
     <main>
-        <article id="genre">genres</article>
-        <article id="tops">top</article>
-        <article id="discover">découverte</article>
+        <article id="genre"></article>
+        <article id="tops">
+            <div id="albums">
+                
+            </div>
+            <div id="tracks">
+
+            </div>
+        </article>
+        <article id="discover"></article>
         <!-- ... -->
     </main>
+    <script src="script.js" async=""></script>
     <!-- end main -->
     
--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-<<<<<<< HEAD
-const dataApi = fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre");
+const dataApiAlbums = fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums");
 
-dataApi
+dataApiAlbums
   .then(async (responseData) => {
     console.log(responseData);
 
     const response = await responseData.json();
-    // const response = await responseData;
-    console.log(response);
-    let genre = document.querySelector('#genre');
-    try {
-      for (let i = 1; i < 11 ; i++) {
-        const dataPicture = response.data[i].picture;
-        const dataName = response.data[i].name;
-        let myImg = document.createElement('img');
-        let myTitle = document.createElement('p');
-        let myGenre = document.createElement('div');
-        myImg.src = datataPicture;
-        myTitle.textContent = datataName;
-        myGenre.appendChild(myImg);
-        myGenre.appendChild(myTitle);
-        genre.appendChild(myGenre);
-      }
 
+    console.log(response);
+    try {
+      for (let i = 0; i < 10 ; i++) {
+        let position = response.data[i].position;
+        let urlThumb = response.data[i].cover_small;
+        let titleTrack = response.data[i].artist.name;
+        let artistName = response.data[i].title;
+        let duration = 0;
+        let line = createLine(position, urlThumb, titleTrack, artistName, duration);
+        addLineToListTopAlbum(line);
+
+      }
     } catch (err) {
       console.log(err);
     }
@@ -31,8 +26,60 @@ dataApi
   .catch((err) => {
     console.log(err)
   })
-=======
-window.addEventListener('load', (event) => {
-    console.log('js work !');
-});
->>>>>>> c51b3a276506362956e0124829cc75b824e54a3a
+
+  const dataApiTracks = fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks");
+
+  dataApiTracks
+  .then(async (responseData) => {
+    console.log(responseData);
+
+    const response = await responseData.json();
+
+    console.log(response);
+    try {
+      for (let i = 0; i < 10 ; i++) {
+
+        let position = response.data[i].position;
+        let urlThumb = response.data[i].artist.picture_small;
+        let artistName = response.data[i].artist.name;
+        let titleTrack = response.data[i].title;
+        let duration = 0;
+        let line = createLine(position, urlThumb, titleTrack, artistName, duration);
+        addLineToListTopTracks(line);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+
+  .catch((err) => {
+    console.log(err)
+  })
+
+  function createLine(nbr, urlThumb, titleTrack, artistName, duration) {
+
+    let list = '<div class="lineTop">';
+    list += '<div class="leftLine">';
+    list += '    <div class="nbr">'+nbr+'</div>';
+    list += '    <div class="thumbAlbumTopTracks"><img src="'+urlThumb+'" alt="thumb Album"></div>';
+    list += '    < class="trackInfos">';
+    list += '        <div class="TitleTrack">'+titleTrack+'</div>';
+    list += '        <div class="artistName blue">'+artistName+'</div>';
+    list += '    </div>';
+    list += '</div>';
+    list += '<div class="timer">'+duration+'</div>';
+    list += '</div>';
+
+    return list;
+}
+
+function addLineToListTopAlbums(line){
+    let listAlbums = document.getElementById('listAlbums');
+
+    listAlbums.innerHTML += line;
+}
+function addLineToListTopTracks(line){
+    let listTracks = document.getElementById('listTracks');
+
+    listTracks.innerHTML += line;
+}
